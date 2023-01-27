6c4df9016e21808a15dfd07333046ccab9946b75
Tian <87069680+Tiaansu@users.noreply.github.com>
2023-01-27 14:04:04 +0800

    Add files via upload

---
+++ FindTheLargestNumber/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FindTheLargestNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputValue;
+            int firstValue, 
+                secondValue, 
+                thirdValue
+            ;
+
+            Console.WriteLine("Find the largest of three numbers:");
+            Console.WriteLine("----------------------------------");
+
+            Console.Write("Input the 1st number: ");
+            inputValue = Console.ReadLine();
+            firstValue = Convert.ToInt32(inputValue);
+
+            Console.Write("Input the 2nd number: ");
+            inputValue = Console.ReadLine();
+            secondValue = Convert.ToInt32(inputValue);
+
+            Console.Write("Input the 3rd number: ");
+            inputValue = Console.ReadLine();
+            thirdValue = Convert.ToInt32(inputValue);
+
+            if (firstValue > thirdValue && firstValue > secondValue)
+            {
+                if (firstValue > thirdValue)
+                {
+                    Console.WriteLine("The 1st number is the greatest among three.");
+                }
+                else
+                {
+                    Console.WriteLine("The 3rd number is the greatest among three.");
+                }
+            }
+            else
+            {
+                if (secondValue > thirdValue)
+                {
+                    Console.WriteLine("The 2nd number is the greatest among three.");
+                }
+                else
+                {
+                    Console.WriteLine("The 3rd number is the greatest among three.");
+                }
+            }
+        }
+    }
+}
